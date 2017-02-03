c8204b397f33a58c784bf14f78844534360cfbb9
Amit Moryossef <amitmoryossef@gmail.com>
2017-02-03 15:00:07 +0200

    Now fully renders the properties as a markdown table

--- README-header.md
+++ README-header.md
@@ -8,9 +8,14 @@ A grid comparison standard
 ## Contribution
 If you want to add a grid, please create a pull request of a new file in `grids`. We have automated testing for you to know if everything was done correctly.
 If you want to change information for a grid, you must also reference a legitimate source for it.
-
+If we are missing a grid feature, please create an issue, and we will consider adding it.
 ---
 
 ## Shallow Comparison
 Here we compare the grids using the markdown table, to show no bias to any grid. If willing, other grids can show this table using their grid.
 This comparison is alphabetical, to avoid owner's bias.
+
+**Legend**:
+- ![green](http://placehold.it/20/c5f015/000000?text=+) Has.
+- ![red](http://placehold.it/20/f03c15/000000?text=+) Does not have.
+- ![blue](http://placehold.it/20/1589F0/000000?text=+) N/A.
--- src/render.js
+++ src/render.js
@@ -1,5 +1,10 @@
 import fs from "fs";
 
+function isURL(str) {
+    let pattern = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
+    return pattern.test(str);
+}
+
 function getProp(grid, field = "") {
     let fields = field.split(".");
     let param = grid;
@@ -11,11 +16,11 @@ function getProp(grid, field = "") {
 
 class Render {
     static link(str, title = "Link") {
-        return "["+title+"](" + str + ")";
+        return "[" + title + "](" + str + ")";
     }
 
     static image(str, title = "Image") {
-        return "!["+title+"](" + str + ")";
+        return "![" + title + "](" + str + ")";
     }
 
     static stars(str) {
@@ -26,8 +31,20 @@ class Render {
         return Render.image("assets/frameworks/" + str.toLowerCase() + ".png", str);
     }
 
-    static array(mapper) {
-        return (arr) => arr.map(mapper).join(", ");
+    static array(mapper = null) {
+        return (arr) => {
+            if (mapper !== null)
+                arr = arr.map(mapper);
+            return arr.join(", ");
+        };
+    }
+
+    static arrayOrNull(mapper = null) {
+        return (arr) => {
+            if (arr === null)
+                return Render.boolStringNull(false);
+            return Render.array(mapper)(arr);
+        };
     }
 
     static color(color) {
@@ -43,7 +60,7 @@ class Render {
                 c = "1589F0";
         }
 
-        return "!["+color+"](http://placehold.it/20/"+c+"/000000?text=+)"
+        return "![" + color + "](http://placehold.it/20/" + c + "/000000?text=+)"
     }
 
     static boolStringNull(any) {
@@ -51,19 +68,19 @@ class Render {
             return Render.link(any);
 
         if (any === null)
-            return Render.color("yellow");
+            return Render.color("blue");
 
         if (any === true)
             return Render.color("green");
 
-        return this.color("red");
+        return Render.color("red");
     }
 }
 
-let clearRow = {
-    title: "-",
-    renderer: () => "-"
-};
+let clearRow = (title = "-") => new Object({
+    title: title,
+    renderer: () => ""
+});
 
 const rows = [
     {
@@ -77,7 +94,7 @@ const rows = [
     }, {
         title: "Description",
         field: "info.description"
-    },{
+    }, {
         title: "License",
         field: "info.license"
     }, {
@@ -99,7 +116,179 @@ const rows = [
         title: "Demo",
         field: "info.website.demo",
         renderer: Render.link
-    }, clearRow
+    }, clearRow(), clearRow("General"), {
+        title: "Pivot Mode",
+        field: "features.pivoting",
+        renderer: Render.boolStringNull
+    }, {
+        title: "Print Mode",
+        field: "features.print",
+        renderer: Render.boolStringNull
+    }, {
+        title: "Export",
+        field: "features.export",
+        renderer: Render.array()
+    }, {
+        title: "Pagination",
+        field: "features.pagination",
+        renderer: Render.boolStringNull
+    }, {
+        title: "Virtual Pagination",
+        field: "features.virtualPagination",
+        renderer: Render.boolStringNull
+    }, clearRow(), clearRow("Rows"), {
+        title: "Rows Grouping",
+        field: "features.rows.grouping",
+        renderer: Render.boolStringNull
+    }, {
+        title: "Floating Rows",
+        field: "features.rows.floating",
+        renderer: Render.boolStringNull
+    }, {
+        title: "Full Width Rows",
+        field: "features.rows.fullWidth",
+        renderer: Render.boolStringNull
+    }, {
+        title: "Rows Numbering",
+        field: "features.rows.numbering",
+        renderer: Render.boolStringNull
+    }, {
+        title: "Rows Selection",
+        field: "features.rows.selection",
+        renderer: Render.boolStringNull
+    }, {
+        title: "Virtual DOM",
+        field: "features.rows.virtualDOM",
+        renderer: Render.boolStringNull
+    }, {
+        title: "Dynamic Add Rows",
+        field: "features.rows.dynamicInsert",
+        renderer: Render.boolStringNull
+    }, {
+        title: "Dynamic Remove Rows",
+        field: "features.rows.dynamicRemove",
+        renderer: Render.boolStringNull
+    }, {
+        title: "Dynamic Row Height",
+        field: "features.rows.dynamicHeight",
+        renderer: Render.boolStringNull
+    }, clearRow(), clearRow("Columns"), {
+        title: "Filtering",
+        field: "features.columns.filtering",
+        renderer: Render.boolStringNull
+    }, {
+        title: "Filters",
+        field: "features.columns.customFilters",
+        renderer: Render.array()
+    }, {
+        title: "Sorting",
+        field: "features.columns.sorting",
+        renderer: Render.boolStringNull
+    }, {
+        title: "Pinning",
+        field: "features.columns.pinning",
+        renderer: Render.boolStringNull
+    }, {
+        title: "Reordering",
+        field: "features.columns.reorder",
+        renderer: Render.boolStringNull
+    }, {
+        title: "Resizing",
+        field: "features.columns.resizing",
+        renderer: Render.boolStringNull
+    }, {
+        title: "Header Grouping",
+        field: "features.columns.grouping",
+        renderer: Render.boolStringNull
+    }, {
+        title: "Selection",
+        field: "features.columns.selection",
+        renderer: Render.boolStringNull
+    }, {
+        title: "Validation",
+        field: "features.columns.validation",
+        renderer: Render.boolStringNull
+    }, {
+        title: "Control Menu",
+        field: "features.columns.controlMenu",
+        renderer: Render.boolStringNull
+    }, {
+        title: "Aggregation",
+        field: "features.columns.aggregation",
+        renderer: Render.arrayOrNull()
+    }, {
+        title: "Header Rendering",
+        field: "features.columns.headerRendering",
+        renderer: Render.boolStringNull
+    }, clearRow(), clearRow("Cells"), {
+        title: "Custom Rendering",
+        field: "features.cells.customRendering",
+        renderer: Render.boolStringNull
+    }, {
+        title: "Formula Support",
+        field: "features.cells.formula",
+        renderer: Render.boolStringNull
+    }, {
+        title: "Inline Editing",
+        field: "features.cells.editing",
+        renderer: Render.boolStringNull
+    }, {
+        title: "Custom styling",
+        field: "features.cells.styling",
+        renderer: Render.boolStringNull
+    }, {
+        title: "Clipboard",
+        field: "features.cells.clipboard",
+        renderer: Render.boolStringNull
+    }, {
+        title: "Keyboard Navigation",
+        field: "features.cells.keyboardNavigation",
+        renderer: Render.arrayOrNull()
+    }, {
+        title: "Range Selection",
+        field: "features.cells.rangeSelection",
+        renderer: Render.boolStringNull
+    }, {
+        title: "Merge Cells",
+        field: "features.cells.merge",
+        renderer: Render.boolStringNull
+    }, clearRow(), clearRow("Nice To Have"), {
+        title: "Animations",
+        field: "features.animations",
+        renderer: Render.boolStringNull
+    }, {
+        title: "Custom Icons",
+        field: "features.customIcons",
+        renderer: Render.boolStringNull
+    }, {
+        title: "Custom Overlays",
+        field: "features.customOverlays",
+        renderer: Render.boolStringNull
+    }, {
+        title: "Global Search",
+        field: "features.globalSearch",
+        renderer: Render.boolStringNull
+    }, {
+        title: "Internationalisation",
+        field: "features.internationalisation",
+        renderer: Render.boolStringNull
+    }, {
+        title: "Master/Slave",
+        field: "features.masterSlave",
+        renderer: Render.boolStringNull
+    }, {
+        title: "RTL Support",
+        field: "features.RTLSupport",
+        renderer: Render.boolStringNull
+    }, {
+        title: "Footer",
+        field: "features.statusBar",
+        renderer: Render.boolStringNull
+    }, {
+        title: "Touch support",
+        field: "features.touchSupport",
+        renderer: Render.boolStringNull
+    }
 ];
 
 function createRow(array, title = "") {
@@ -109,6 +298,9 @@ function createRow(array, title = "") {
 function createTable(data) {
     let table = [];
 
+    // Sort by name
+    data = data.sort((a, b) => a.info.name < b.info.name);
+
     table.push(createRow(data.map(grid => grid.info.name), "/"));
     table.push(Array.from(table[0]).map(c => (c == "|") ? "|" : "-").join(""));
 
@@ -116,6 +308,10 @@ function createTable(data) {
         table.push(createRow(data.map(grid => getProp(grid, param.field)).map((item) => {
             if (param.renderer)
                 return param.renderer(item, param.title);
+
+            if (isURL(item))
+                return Render.link(item, param.title);
+
             return item;
         }), param.title));
     });
